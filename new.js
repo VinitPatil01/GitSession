4001d3016745f7de1abdaa3cb41a139ab4404b1c
VinitPatil01 <pvinit97@gmail.com>
2025-05-02 16:30:44 +0530

    changed new.js

--- new.js
+++ new.js
@@ -1,7 +1,12 @@
 function hello() {
-    console.log("changed hello");
+    console.log("i couldnot understand shit about merging");
 }
 
 function apppend(){
     console.log("practicing git")
+}
+
+
+const newF = ()=>{
+    console.log("merging conflict")
 }
